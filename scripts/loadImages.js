9d8684273ebe9fce560d638f6dd9826fc8e79e57
Frank Showalter <fshowalter@gmail.com>
2020-05-03 00:18:43 -0400

    Updates

---
+++ scripts/loadImages.js
@@ -0,0 +1,19 @@
+"use strict";
+(function initUpdateImages() {
+    function removePlaceholderForLoadedImage() {
+        const placeholder = this.previousSibling;
+        if (!placeholder) {
+            return;
+        }
+        placeholder.remove();
+    }
+    function updateImages() {
+        let i;
+        const nodes = document.querySelectorAll("<picture>");
+        const nodesLength = nodes.length;
+        for (i = 0; i < nodesLength; i += 1) {
+            nodes[i].addEventListener("load", removePlaceholderForLoadedImage);
+        }
+    }
+    updateImages();
+})();
